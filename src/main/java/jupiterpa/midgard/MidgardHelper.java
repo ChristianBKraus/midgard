0b798cb63af3ddf0dd5fd8da2b59142ed484bbd1
jupiterpi <jupiterpi@outlook.de>
2018-04-29 20:27:45 +0200

    Ich habe einen Anfang abgespeichert.

--- src/main/java/jupiterpa/midgard/MidgardHelper.java
+++ src/main/java/jupiterpa/midgard/MidgardHelper.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MidgardHelper {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MidgardHelper.class, args);
 		System.out.println("Test");
 	} 
 }
